f43b99aa4ae36238869f47f38020a285a8663f3a
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-10 13:50:30 +0100

    Add Deck entity

---
+++ CardGame.Lib/Models/Deck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Lib.Models
+{
+    class Deck : BaseEntity
+    {
+        public string Name { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
--- CardGame.Lib/Models/User.cs
+++ CardGame.Lib/Models/User.cs
@@ -10,5 +10,6 @@ namespace CardGame.Lib.Models
         public string Email { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
+        public ICollection<Deck> Decks { get; set; }
     }
 }
