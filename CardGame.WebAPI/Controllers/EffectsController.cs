e6dc08724991164d19f0af03a4f684a5b89226fc
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 15:00:14 +0100

    Add EffectsController

---
+++ CardGame.WebAPI/Controllers/EffectsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CardGame.Lib.Models;
+using CardGame.WebAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardGame.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EffectsController : BaseCrudController<Effect, EffectRepository>
+    {
+        public EffectsController(EffectRepository effectRepository) : base(effectRepository)
+        {
+        }
+    }
+}
