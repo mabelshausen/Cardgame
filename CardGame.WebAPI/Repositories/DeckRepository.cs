c55dbc0afd068dd2900fe6eb0268698a457f60d1
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 14:21:18 +0100

    Add DeckRepository

---
+++ CardGame.WebAPI/Repositories/DeckRepository.cs
@@ -0,0 +1,16 @@
+﻿using CardGame.Lib.Models;
+using CardGame.WebAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.WebAPI.Repositories
+{
+    public class DeckRepository : BaseRepository<Deck>
+    {
+        public DeckRepository(CardGameContext cardGameContext) : base(cardGameContext)
+        {
+        }
+    }
+}
