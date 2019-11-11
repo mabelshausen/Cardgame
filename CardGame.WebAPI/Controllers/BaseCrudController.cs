cedff981c02c31bbe7cd113b7e61cd0f337a6986
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 14:04:18 +0100

    Add Crud methods to BaseCrudController

--- CardGame.WebAPI/Controllers/BaseCrudController.cs
+++ CardGame.WebAPI/Controllers/BaseCrudController.cs
@@ -18,5 +18,69 @@ namespace CardGame.WebAPI.Controllers
         {
             _repository = repository;
         }
+
+        [HttpGet]
+        public virtual async Task<IActionResult> Get()
+        {
+            return Ok(await _repository.ListAll());
+        }
+
+        [HttpGet("{id}")]
+        public virtual async Task<IActionResult> Get(int id)
+        {
+            return Ok(await _repository.GetById(id));
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> Post([FromBody] T entity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            T createdEntity = await _repository.Add(entity);
+            if (createdEntity == null)
+            {
+                return NotFound();
+            }
+            return CreatedAtAction(nameof(Get), new { id = entity.Id }, entity);
+        }
+
+        [HttpDelete("{id}")]
+        public virtual async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            T deletedEntity = await _repository.Delete(id);
+            if (deletedEntity == null)
+            {
+                return NotFound();
+            }
+            return Ok(deletedEntity);
+        }
+
+        [HttpPut("{id}")]
+        public virtual async Task<IActionResult> Put([FromRoute] int id, [FromBody] T entity)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (id != entity.Id)
+            {
+                return BadRequest();
+            }
+
+            T updatedEntity = await _repository.Update(entity);
+            if (updatedEntity == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedEntity);
+        }
     }
 }
