475b70b058df2d4eab7d824aad54dc192eca924c
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 14:21:31 +0100

    Add CardRepository

---
+++ CardGame.WebAPI/Repositories/CardRepository.cs
@@ -0,0 +1,16 @@
+﻿using CardGame.Lib.Models;
+using CardGame.WebAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.WebAPI.Repositories
+{
+    public class CardRepository : BaseRepository<Card>
+    {
+        public CardRepository(CardGameContext cardGameContext) : base(cardGameContext)
+        {
+        }
+    }
+}
