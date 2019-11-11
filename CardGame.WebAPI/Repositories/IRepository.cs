a4d565bcb95ab3d39affcdc36171b774699bdca4
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 13:18:57 +0100

    Add IRepository interface

---
+++ CardGame.WebAPI/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using CardGame.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.WebAPI.Repositories
+{
+    interface IRepository<T> where T : BaseEntity
+    {
+        Task<T> GetById(int id);
+        IQueryable<T> GetAll();
+        Task<IEnumerable<T>> ListAll();
+        Task<T> Add(T entity);
+        Task<T> Delete(T entity);
+        Task<T> Delete(int id);
+        Task<T> Update(T entity);
+    }
+}
