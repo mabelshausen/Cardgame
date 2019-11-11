019c6bf3e9370903264be4971f832d3d435aa2f2
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 14:21:43 +0100

    Add MonsterRepository

---
+++ CardGame.WebAPI/Repositories/MonsterRepository.cs
@@ -0,0 +1,16 @@
+﻿using CardGame.Lib.Models;
+using CardGame.WebAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.WebAPI.Repositories
+{
+    public class MonsterRepository : BaseRepository<Monster>
+    {
+        public MonsterRepository(CardGameContext cardGameContext) : base(cardGameContext)
+        {
+        }
+    }
+}
