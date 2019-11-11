c0d330b1ebda5327e87be09cd7f688ada0a3224d
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 13:48:55 +0100

    Add BaseCrudController class

---
+++ CardGame.WebAPI/Controllers/BaseCrudController.cs
@@ -0,0 +1,22 @@
+﻿using CardGame.Lib.Models;
+using CardGame.WebAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.WebAPI.Controllers
+{
+    public class BaseCrudController<T, R> : ControllerBase 
+        where T : BaseEntity 
+        where R : BaseRepository<T>
+    {
+        protected R _repository;
+
+        public BaseCrudController(R repository)
+        {
+            _repository = repository;
+        }
+    }
+}
