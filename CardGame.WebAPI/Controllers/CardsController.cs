ae54d6f6468f7280d455f51f3e5dd9363beddb1d
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 14:57:11 +0100

    Add CardsController

---
+++ CardGame.WebAPI/Controllers/CardsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CardGame.Lib.Models;
+using CardGame.WebAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardGame.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : BaseCrudController<Card, CardRepository>
+    {
+        public CardsController(CardRepository cardRepository) : base(cardRepository)
+        {
+        }
+    }
+}
