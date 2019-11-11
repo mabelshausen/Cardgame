65df009707a1f49890adf4d6bf9bb32e2fb6403c
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 14:46:45 +0100

    Add JsonIgnore attribute to Card.Effects

--- CardGame.Lib/Models/Card.cs
+++ CardGame.Lib/Models/Card.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -9,6 +10,7 @@ namespace CardGame.Lib.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public ICollection<DeckCards> DeckCards { get; set; }
+        [JsonIgnore]
         public ICollection<Effect> Effects { get; set; }
     }
 }
