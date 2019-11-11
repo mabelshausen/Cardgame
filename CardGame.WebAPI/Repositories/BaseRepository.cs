9e76771e38c75c95325f0acbbd551678c89a894f
Marijn Abelshausen <m.abelshausen@gmail.com>
2019-11-11 13:23:51 +0100

    Add BaseRepository class

---
+++ CardGame.WebAPI/Repositories/BaseRepository.cs
@@ -0,0 +1,54 @@
+﻿using CardGame.Lib.Models;
+using CardGame.WebAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.WebAPI.Repositories
+{
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        protected readonly CardGameContext _cardGameContext;
+
+        public BaseRepository(CardGameContext cardGameContext)
+        {
+            _cardGameContext = cardGameContext;
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> ListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
