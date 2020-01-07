58d83c4586ef4e27110fce959eb46069422ff3ff
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-07 13:42:36 +0100

    Add MVC project

---
+++ CardGame.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CardGame.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
