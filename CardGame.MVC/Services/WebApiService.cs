0cbc190c6a91e013340f705e023f3befd8f9f886
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-07 14:30:10 +0100

    Add WepApiService

---
+++ CardGame.MVC/Services/WebApiService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace CardGame.MVC.Services
+{
+    public class WebApiService
+    {
+        public static T GetApiResult<T>(string uri)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                Task<string> response = httpClient.GetStringAsync(uri);
+                return Task.Factory.StartNew(
+                    () => JsonConvert.DeserializeObject<T>(response.Result)
+                ).Result;
+            }
+        }
+
+        public static async Task<TOut> PutCallApi<TOut, TIn>(string uri, TIn entity)
+        {
+            return await CallApi<TOut, TIn>(uri, entity, HttpMethod.Put);
+        }
+
+        public static async Task<TOut> PostCallApi<TOut, TIn>(string uri, TIn entity)
+        {
+            return await CallApi<TOut, TIn>(uri, entity, HttpMethod.Post);
+        }
+
+        public static async Task<TOut> DeleteCallApi<TOut>(string uri)
+        {
+            return await CallApi<TOut, object>(uri, null, HttpMethod.Delete);
+        }
+
+        private static async Task<TOut> CallApi<TOut, TIn>(string uri, TIn entity, HttpMethod httpMethod)
+        {
+            TOut result = default(TOut);
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                HttpResponseMessage response;
+                if (httpMethod == HttpMethod.Post)
+                {
+                    response = await httpClient.PostAsJsonAsync(uri, entity);
+                }
+                else if (httpMethod == HttpMethod.Put)
+                {
+                    response = await httpClient.PutAsJsonAsync(uri, entity);
+                }
+                else
+                {
+                    response = await httpClient.DeleteAsync(uri);
+                }
+                result = await response.Content.ReadAsAsync<TOut>();
+            }
+            return result;
+        }
+    }
+}
