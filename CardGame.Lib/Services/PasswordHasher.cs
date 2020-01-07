d08a86aca3bcff95be6e609caacb585fb88cc9fa
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-07 13:51:18 +0100

    Add passwordhasher class

---
+++ CardGame.Lib/Services/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Lib.Services
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
