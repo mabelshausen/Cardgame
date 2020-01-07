58c256619109ea7001f704ed383303f15a74486c
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-07 15:03:23 +0100

    Add CardsController with Index view

---
+++ CardGame.MVC/Controllers/CardsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardGame.MVC.Controllers
+{
+    public class CardsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
