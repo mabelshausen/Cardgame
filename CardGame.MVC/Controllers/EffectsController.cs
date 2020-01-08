2943cc15db20d11330faaa7687eb08ede14cae37
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-08 14:45:47 +0100

    Add Effects controller and Index view

---
+++ CardGame.MVC/Controllers/EffectsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardGame.MVC.Controllers
+{
+    public class EffectsController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+    }
+}
