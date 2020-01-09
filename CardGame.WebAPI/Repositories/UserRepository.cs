2f43d485de9d26f41e66130636814e2f712a480f
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 12:42:05 +0100

    Add override methods to map User to UserDto

--- CardGame.WebAPI/Repositories/UserRepository.cs
+++ CardGame.WebAPI/Repositories/UserRepository.cs
@@ -1,5 +1,9 @@
-﻿using CardGame.Lib.Models;
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CardGame.Lib.Dto;
+using CardGame.Lib.Models;
 using CardGame.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +11,23 @@ using System.Threading.Tasks;
 
 namespace CardGame.WebAPI.Repositories
 {
-    public class UserRepository : BaseRepository<User>
+    public class UserRepository : MappingRepository<User>
     {
-        public UserRepository(CardGameContext cardGameContext) : base(cardGameContext)
+        public UserRepository(CardGameContext cardGameContext, IMapper mapper) : base(cardGameContext, mapper)
+        { 
+        }
+
+        public async Task<UserDto> GetDetailById(int id)
         {
+            return _mapper.Map<UserDto>( await _cardGameContext.Set<User>()
+                .FirstOrDefaultAsync(e => e.Id == id && !e.IsDeleted));
+        }
+
+        public async Task<IEnumerable<UserDto>> ListAllDetail()
+        {
+            return await GetAll()
+                .ProjectTo<UserDto>(_mapper.ConfigurationProvider)
+                .ToListAsync();
         }
     }
 }
