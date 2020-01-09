5890d99f8bf01f9358ed53d3f4e480c5313fdfbf
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 13:34:29 +0100

    Add UserState Session key

---
+++ CardGame.MVC/Constants/UserStateConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.MVC.Constants
+{
+    public static class UserStateConstants
+    {
+        public const string UserStateSessionKey = "SessionUser";
+    }
+}
