13b0f5f610a120e4d118cf6160920fc7d3a9cee5
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 12:28:16 +0100

    Add UserDto

---
+++ CardGame.Lib/Dto/UserDto.cs
@@ -0,0 +1,16 @@
+﻿using CardGame.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardGame.Lib.Dto
+{
+    class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool IsAdmin { get; set; }
+        public ICollection<Deck> Decks { get; set; }
+    }
+}
