66be202210cc3d792eb5c9787b45715fe82b5641
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 13:22:27 +0100

    Add LoginViewModel

---
+++ CardGame.MVC/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.MVC.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string RepeatPassword { get; set; }
+    }
+}
