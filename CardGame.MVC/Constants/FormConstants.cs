e281a66d40cfa67944f81d30ad4737dc4904945f
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 13:30:45 +0100

    Add FormConstants

---
+++ CardGame.MVC/Constants/FormConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.MVC.Constants
+{
+    public static class FormConstants
+    {
+        public const string NameDisplay = "Username";
+        public const string NameRequired = "Username is required.";
+
+        public const string PasswordDisplay = "Password";
+        public const string PasswordRequired = "Password is required.";
+
+        public const string RepeatPasswordDisplay = "Repeat Password";
+        public const string RepeatPasswordRequired = "Repeated Password is required.";
+        public const string RepeatPasswordNotEqual = "Passwords do not match.";
+    }
+}
