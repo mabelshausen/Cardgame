f7d6a5c20a8fbbe77c0880f8868e3f0d809ab257
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 12:33:53 +0100

    Add AutoMapperProfileConfiguration

---
+++ CardGame.WebAPI/Services/AutoMapperProfileConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using CardGame.Lib.Dto;
+using CardGame.Lib.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.WebAPI.Services
+{
+    public class AutoMapperProfileConfiguration : Profile
+    {
+        public AutoMapperProfileConfiguration() : this("MyProfile")
+        {
+
+        }
+
+        public AutoMapperProfileConfiguration(string profileName) : base(profileName)
+        {
+            CreateMap<UserDto, User>().ReverseMap();
+        }
+    }
+}
