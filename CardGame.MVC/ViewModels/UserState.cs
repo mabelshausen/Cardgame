882a8044576367d9a01c78e60544b79b941ae40f
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 13:35:56 +0100

    Add userstate model

---
+++ CardGame.MVC/ViewModels/UserState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.MVC.ViewModels
+{
+    public class UserState
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsLoggedIn { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
