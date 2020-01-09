2bb34e1e20d06a484821902cda056788ca0d5355
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 12:37:20 +0100

    Add MappingRepository

---
+++ CardGame.WebAPI/Repositories/MappingRepository.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CardGame.Lib.Models;
+using CardGame.WebAPI.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CardGame.WebAPI.Repositories
+{
+    public class MappingRepository<T> : BaseRepository<T> where T : BaseEntity
+    {
+        protected readonly IMapper _mapper;
+
+        public MappingRepository(CardGameContext cardGameContext, IMapper mapper) : base(cardGameContext)
+        {
+            _mapper = mapper;
+        }
+    }
+}
