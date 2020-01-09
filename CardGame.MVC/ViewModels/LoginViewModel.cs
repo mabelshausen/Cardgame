76c12783e4ec1a3c1a7fb6a9da9574391a1f657e
Marijn Abelshausen <m.abelshausen@gmail.com>
2020-01-09 13:30:59 +0100

    Add data annotations to LoginViewModel

--- CardGame.MVC/ViewModels/LoginViewModel.cs
+++ CardGame.MVC/ViewModels/LoginViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CardGame.MVC.Constants;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +9,19 @@ namespace CardGame.MVC.ViewModels
 {
     public class LoginViewModel
     {
+        [Display(Name = FormConstants.NameDisplay)]
+        [Required(ErrorMessage = FormConstants.NameRequired)]
         public string Name { get; set; }
+
+        [Display(Name = FormConstants.PasswordDisplay)]
+        [Required(ErrorMessage = FormConstants.PasswordRequired)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
+
+        [Display(Name = FormConstants.RepeatPasswordDisplay)]
+        [Required(ErrorMessage = FormConstants.RepeatPasswordRequired)]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password), ErrorMessage = FormConstants.RepeatPasswordNotEqual)]
         public string RepeatPassword { get; set; }
     }
 }
